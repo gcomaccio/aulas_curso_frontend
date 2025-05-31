eec4833641945a4c8046534840f6ae4b01c2135e
gcomaccio <gcomaccio@gmail.com>
2025-05-31 12:43:37 -0300

    Aula Java Script

--- script-01.js
+++ script-01.js
@@ -1,3 +1,9 @@
 function dataHora(){
         document.getElementById('demo').innerHTML = Date();
 }
+function dataHora(){
+        document.getElementById('demo2').innerHTML = Date() + ", legal!!!";
+    }
+    function msg(id, msg){
+        document.getElementById(id).innerHTML = msg;
+    }
--- script-02.js
+++ script-02.js
@@ -1,6 +1,7 @@
 function soma(){
-  var a = document.getElementById('valor01').value;
-  var b = document.getElementById('valor02').value;
+  var a = parseFloat (document.getElementById('valor01').value);
+  var b = parseFloat (document.getElementById('valor02').value);
   document.getElementById('demo').innerHTML = a + b;
   //document.getElementById('demo').innerHTML = parseInt(a) + parseInt(b);
+
 }
