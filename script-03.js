d2539dbf3c680c4d9177bbd73ed23c1345109c91
gcomaccio <gcomaccio@gmail.com>
2025-06-07 12:13:54 -0300

    Update Java

---
+++ script-03.js
@@ -0,0 +1,14 @@
+function maiorValor(){
+  var a = parseInt(document.getElementById('valor01').value);
+  var b = parseInt(document.getElementById('valor02').value);
+  var c = parseInt(document.getElementById('valor03').value);
+ 
+  if(){
+    document.getElementById('demo').innerHTML = "É um número ímpar.";
+  }else if (){
+    document.getElementById('demo').innerHTML = "É um número par.";
+  }else{
+
+  }
+
+}
